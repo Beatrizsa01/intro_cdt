2153b7695bece69b83fd650834e5abf5fcfda65e
Beatriz Souza Araujo <beatrizsouzaaraujo173@gmail.com>
2026-07-08 12:26:15 -0300

    upgrade

---
+++ beatriz.py
@@ -0,0 +1,14 @@
+'''
+Módulo 4 - Estrutura de dados - Lista
+'''
+
+frutas_maduras = ['banana', 'morango', 'manga', 'abacaxi', 'melancia']
+print('🍓' * 40)
+print('Seja bem-vindo(a) ao sistema de listas  de Frutas Maduras! \n')
+print('🍓' * 40)
+
+fruta_escolhida = input('Digite o nome da fruta que deseja colocar: ')
+
+
+frutas_maduras.append(fruta_escolhida)
+
